a42021b6f1379c6e81ce7a67add084b63e45848b
Matt <matt@mspacecreative.com>
2019-04-23 12:51:02 -0300

    c

--- wp-content/themes/nodalblock/functions.php
+++ wp-content/themes/nodalblock/functions.php
@@ -140,14 +140,14 @@ if (function_exists('add_theme_support')) {
 	add_image_size('small', 120, '', true); // Small Thumbnail
 }
 
-/* STYLE GUTENBERG 
+/* STYLE GUTENBERG */
 function gutenberg_editor_styles() {
 	add_theme_support('editor-styles');
 	add_editor_style( 'css/gutenberg.css' );
-}*/
+}
 
 // ACTIONS, OPTIONS AND FILTERS
 add_action('wp_enqueue_scripts', 'nodal_enqueue_styles');
 add_action( 'after_setup_theme', 'nav_registration' );
 add_action('acf/init', 'my_acf_init');
-//add_action('init','gutenberg_editor_styles');
+add_action('init','gutenberg_editor_styles');
