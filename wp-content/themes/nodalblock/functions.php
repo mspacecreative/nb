040740993aef40ca8a2fab9b114522b79ed24928
Matt <matt@mspacecreative.com>
2019-04-23 12:38:34 -0300

    c

--- wp-content/themes/nodalblock/functions.php
+++ wp-content/themes/nodalblock/functions.php
@@ -18,11 +18,11 @@ function nodal_enqueue_styles() {
 	wp_register_style( 'parallaxer-styles', get_template_directory_uri() . '/css/parallaxer.css', '', null );
 	wp_enqueue_style('parallaxer-styles');
 	
-	//wp_register_script('js-vendor', get_template_directory_uri() . '/js/vendor/modernizr-2.6.2.min.js', array('jquery'), null);
-	//wp_enqueue_script('js-vendor');
+	wp_register_script('js-vendor', get_template_directory_uri() . '/js/vendor/modernizr-2.6.2.min.js', array('jquery'), null);
+	wp_enqueue_script('js-vendor');
 	
-	wp_register_script('parallaxer-scripts', get_template_directory_uri() . '/js/parallaxer.js', array('jquery'), null, true);
-	wp_enqueue_script('parallaxer-scripts');
+	//wp_register_script('parallaxer-scripts', get_template_directory_uri() . '/js/parallaxer.js', array('jquery'), null, true);
+	//wp_enqueue_script('parallaxer-scripts');
 	
 	wp_register_script('fontawesome', 'https://use.fontawesome.com/6ccd600e51.js', array('jquery'), null, true);
 	wp_enqueue_script('fontawesome');
