b2e1a276268f96560ade4e438598541ae432966d
Matt <matt@mspacecreative.com>
2019-04-23 11:37:26 -0300

    c

--- wp-content/themes/nodalblock/js/main.js
+++ wp-content/themes/nodalblock/js/main.js
@@ -27,14 +27,53 @@ function headerAdjust() {
 	}
 }
 
-$(document).ready(function() {
+jQuery(document).ready(function($) {
 	//navHeight();
 	hamburgerMenu();
 	//digiSectionHeight();
 	dzsprx_init('.parallaxer-container, { direction: "reverse", mode_scroll:"frombottom" }');
+	
+	// SMOOTH SCROLL
+	// Select all links with hashes
+	$('a[href*="#"]')
+	  // Remove links that don't actually link to anything
+	  .not('[href="#"]')
+	  .not('[href="#0"]')
+	  .click(function(event) {
+	    // On-page links
+	    if (
+	      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+	      && 
+	      location.hostname == this.hostname
+	    ) {
+	      // Figure out element to scroll to
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	      // Does a scroll target exist?
+	      if (target.length) {
+	        // Only prevent default if animation is actually gonna happen
+	        event.preventDefault();
+	        $('html, body').animate({
+	          scrollTop: target.offset().top
+	        }, 500, function() {
+	          // Callback after animation
+	          // Must change focus!
+	          var $target = $(target);
+	          $target.focus();
+	          if ($target.is(":focus")) { // Checking if the target was focused
+	            return false;
+	          } else {
+	            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+	            $target.focus(); // Set focus again
+	          };
+	        });
+	      }
+	    }
+	});
+	
 });
 
-$(window).scroll(function() {
+jQuery(window).scroll(function($) {
 	headerAdjust();
 	
 	if ($('header').hasClass('shrink') ) {
@@ -42,44 +81,6 @@ $(window).scroll(function() {
 	} else {
 		$('.navigation').removeClass('shrink');
 	}
-});
-
-// SMOOTH SCROLL
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 500, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
 });
 
 
