1aaff0084edd97dabb9fa809e7ab6c5f7aa9c607
Matt <matt@mspacecreative.com>
2019-04-24 09:44:09 -0300

    c

--- wp-content/themes/nodalblock/footer.php
+++ wp-content/themes/nodalblock/footer.php
@@ -26,9 +26,9 @@
 						<?php if( have_rows('social_media', 'options') ): ?>
 						<ul class="social-media">
 							<?php while( have_rows('social_media', 'options') ): the_row(); ?>
-							<li><a href="<?php the_sub_field('facebook', 'options'); ?>"><i class="fa fa-facebook"></i></a></li>
-							<li><a href="<?php the_sub_field('twitter', 'options'); ?>"><i class="fa fa-twitter"></i></a></li>
-							<li><a href="<?php the_sub_field('linkedin', 'options'); ?>"><i class="fa fa-linkedin"></i></a></li>
+							<li><a href="<?php the_sub_field('facebook', 'options'); ?>" target="_blank"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="<?php the_sub_field('twitter', 'options'); ?>" target="_blank"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="<?php the_sub_field('linkedin', 'options'); ?>" target="_blank"><i class="fa fa-linkedin"></i></a></li>
 							<?php endwhile; ?>
 						</ul>
 						<?php endif; ?>
