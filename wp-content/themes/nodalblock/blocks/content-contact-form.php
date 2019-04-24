e90a76d5f6ad00518849c5cb3c0235438129070f
Matt <matt@mspacecreative.com>
2019-04-24 11:48:16 -0300

    c

--- wp-content/themes/nodalblock/blocks/content-contact-form.php
+++ wp-content/themes/nodalblock/blocks/content-contact-form.php
@@ -1,5 +1,5 @@
 <?php if ( get_field('section_title') ): ?>
-<h2><?php the_field('section_title'); ?></h2>
+<h2 class="top-line-rule"><?php the_field('section_title'); ?></h2>
 <?php endif; ?>
 <div class="inquiry-form" style="margin: 25px 0;">
 	<?php echo do_shortcode('[contact-form-7 id="149" title="Get in Touch"]'); ?>
