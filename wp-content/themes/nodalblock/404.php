48bc876535ce413698ec10efc61f835c52c2c5bd
Matt <matt@mspacecreative.com>
2019-04-25 15:07:16 -0300

    c

--- wp-content/themes/nodalblock/404.php
+++ wp-content/themes/nodalblock/404.php
@@ -22,7 +22,7 @@ get_header();
 					<!-- / PAGE TITLE -->
 					
 					<div style="max-width: 750px; margin: auto;">
-						<p class="centered-text"><?php esc_html_e( 'Sorry, this page does not exist. Please use the navigation menu above to find what you&#8217;re looking for, or click ' ); ?><a href="<?php echo home_url(); ?>">here</a><?php esc_html_e(' to go to the home page'); ?>
+						<p class="centered-text"><?php esc_html_e( 'Sorry, this page does not exist. Please use the navigation menu above to find what you&#8217;re looking for, or click ' ); ?><a href="<?php echo home_url(); ?>">here</a><?php esc_html_e(' to go to the home page.'); ?>
 						</p>
 					</div>
 				</div>
