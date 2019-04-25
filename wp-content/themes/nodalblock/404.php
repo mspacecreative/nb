8fc87db4a60b6e7f07e599495f48de31b2fb9ba9
Matt <matt@mspacecreative.com>
2019-04-25 14:59:22 -0300

    c

---
+++ wp-content/themes/nodalblock/404.php
@@ -0,0 +1,28 @@
+<?php  
+get_header();
+?>
+
+		<!-- HERO -->
+		<?php if ( get_field('page_bg_img') ): ?>
+		<div class="hero-page" style="background-image: url(<?php the_field('page_bg_img'); ?>);">
+		</div>
+		<?php else : ?>
+		<div class="hero-page" style="background-image: url(<?php echo get_template_directory_uri(); ?>/video/nodalblock-poster.jpg);">
+		</div>
+		<?php endif; ?>
+		<!-- / HERO -->
+			
+			<section id="not-found">
+				<div class="inner inner-page">
+					
+					<!-- PAGE TITLE -->
+					<div class="page-title">
+						<h1><?php echo esc_html_e( 'Page Not Found' ); ?></h1>
+					</div>
+					<!-- / PAGE TITLE -->
+					
+					<p class="centered-text"><?php esc_html_e( 'Sorry, this page does not exist. Please use the navigation menu above to find what you&#8217;re looking for, or click here to go to the home page.' ); ?></p>
+				</div>
+			</section>
+		
+		<?php get_footer(); ?>
